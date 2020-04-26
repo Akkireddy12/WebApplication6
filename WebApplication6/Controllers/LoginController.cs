850effac47f7fc37c119cf57b313c8979b2fef08
LENOVO <LENOVO@DESKTOP-4PJM6B4>
2020-04-26 22:10:10 +0530

    Added files

---
+++ WebApplication6/Controllers/LoginController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication6.Models;
+
+namespace WebApplication6.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Authorize(WebApplication6.Models.User userModel)
+        {
+            using (ClinicEntities db = new ClinicEntities())
+            {
+                var userdetails = db.Users.Where(x => x.Email == userModel.Email && x.Pswd == userModel.Pswd).FirstOrDefault();
+                if (userdetails == null)
+                {
+                    userModel.LoginErrorMessage = "Wrong username or password";
+                    return View("Index", userModel);
+                }
+                else
+                {
+                    Session["userID"] = userdetails.UID;
+                    return RedirectToAction("Index", "Demo");
+                }
+            }
+        }
+    }
+}
